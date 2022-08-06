2f62390f9ee2c83b285d37e2428a1ce74e61a531
Bernat Vadell <bvadell@feimsoft.com>
2022-08-06 18:12:54 +0200

    upgrade to .net 6

--- Nmpq.Tests/DeserializationTests.cs
+++ Nmpq.Tests/DeserializationTests.cs
@@ -66,22 +66,21 @@ namespace Nmpq.Tests
         }
 
         // format information and test values taken from http://www.teamliquid.net/forum/viewmessage.php?topic_id=117260&currentpage=3#45
-        [TestCase(new byte[] {0x00}, 0)]
-        [TestCase(new byte[] {0x01}, 0)]
-        [TestCase(new byte[] {0x02}, 1)]
-        [TestCase(new byte[] {0x03}, -1)]
-        [TestCase(new byte[] {0x7e}, 63)]
-        [TestCase(new byte[] {0x7f}, -63)]
-        [TestCase(new byte[] {0x80}, 0, ExpectedException = typeof (EndOfStreamException))]
-        [TestCase(new byte[] {0x80, 0x00}, 0)]
-        [TestCase(new byte[] {0x80, 0x01}, 64)]
-        [TestCase(new byte[] {0x81, 0x01}, -64)]
-        [TestCase(new byte[] {0x82, 0x01}, 65)]
-        [TestCase(new byte[] {0x83, 0x01}, -65)]
-        [TestCase(new byte[] {0x80, 0x02}, 128)]
-        [TestCase(new byte[] {0x81, 0x02}, -128)]
-        [TestCase(new byte[] {0x80, 0x80, 0x80, 0x10}, 16777216)]
-        [TestCase(new byte[] {0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x10}, 4503599627370496)]
+        [TestCase(new byte[] { 0x00 }, 0)]
+        [TestCase(new byte[] { 0x01 }, 0)]
+        [TestCase(new byte[] { 0x02 }, 1)]
+        [TestCase(new byte[] { 0x03 }, -1)]
+        [TestCase(new byte[] { 0x7e }, 63)]
+        [TestCase(new byte[] { 0x7f }, -63)]
+        [TestCase(new byte[] { 0x80, 0x00 }, 0)]
+        [TestCase(new byte[] { 0x80, 0x01 }, 64)]
+        [TestCase(new byte[] { 0x81, 0x01 }, -64)]
+        [TestCase(new byte[] { 0x82, 0x01 }, 65)]
+        [TestCase(new byte[] { 0x83, 0x01 }, -65)]
+        [TestCase(new byte[] { 0x80, 0x02 }, 128)]
+        [TestCase(new byte[] { 0x81, 0x02 }, -128)]
+        [TestCase(new byte[] { 0x80, 0x80, 0x80, 0x10 }, 16777216)]
+        [TestCase(new byte[] { 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x10 }, 4503599627370496)]
         public void Variable_length_integers_are_deserialized_correctly(byte[] bytes, long expectedValue)
         {
             using (var stream = new MemoryStream(bytes))
@@ -93,10 +92,23 @@ namespace Nmpq.Tests
             }
         }
 
+        [TestCase(new byte[] { 0x80 }, 0)]
+        public void Variable_length_integers_are_deserialized_correctly_throws(byte[] bytes, long expectedValue)
+        {
+            Assert.That(() =>
+            {
+                using (var stream = new MemoryStream(bytes))
+                using (var reader = new BinaryReader(stream))
+                {
+                    var value = Starcraft2SerializedData.DeserializeVariableLengthInteger(reader);
+                }
+            }, Throws.TypeOf<EndOfStreamException>());
+        }
+
         [Test]
         public void Unknown_data_type_causes_MpqParsingException()
         {
-            var bytes = new byte[] {0xFF};
+            var bytes = new byte[] { 0xFF };
             using (var stream = new MemoryStream(bytes))
             using (var reader = new BinaryReader(stream))
             {
